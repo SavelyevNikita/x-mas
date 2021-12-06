84d44115b659da0c7cb8de28880404b4a86ba014
SavelyevNikita <nikita1savelyev@yandex.ru>
2021-12-06 14:46:22 +0300

    refactor: migrate appView modules to ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -16,7 +16,6 @@ export interface interData {
 // 0: {source: {…}, author: 'The Associated Press', title: 'China tries to reassure on Evergrande as default fears rise', description: 'China’s central bank has released extra money for …veloper’s $310 billion mountain of debt collapses', url: 'https://abcnews.go.com/Business/wireStory/china-reassure-evergrande-default-fears-rise-81578914', …}
 // 1: {source: {…}, author: 'DALATOU MAMAN Associated Press', title: 'Gunmen attack Niger military base, killing at least 12', description: "Niger's interior ministry says that extremist gunm…r with Burkina Faso, killing at least 12 soldiers", url: 'https://abcnews.go.com/International/wireStory/gunmen-attack-niger-military-base-killing-12-81578913', …}
 
-
 export class AppView {
     news: News;
     sources: Sources;
